5bfab040197dc72ccd004e80afc3b3ea2238253c
Josh Perry <joshperry@outlook.com>
2025-12-04 11:58:59 +0000

    Day 3 - part 2

--- AoC25/Days/Day3.cs
+++ AoC25/Days/Day3.cs
@@ -15,37 +15,42 @@ public class Day3 : IDay
 
         return batteryBanks;
     }
-    
+
     public string Part1(string input)
     {
         var batteryBanks = ParseInput(input);
-        var totalJoltage = 0;
-
-        static int FindJoltage(List<int> batteryBank)
-        {
-            var highestJoltage = -1;
-            
-            for (var i = 0; i < batteryBank.Count - 1; i++)
-            {
-                for (var j = i + 1; j < batteryBank.Count; j++)
-                {
-                    highestJoltage = Math.Max(highestJoltage, int.Parse($"{batteryBank[i]}{batteryBank[j]}"));
-                }
-            }
-            
-            return highestJoltage;
-        }
-
-        foreach (var batteryBank in batteryBanks)
-        {
-            totalJoltage += FindJoltage(batteryBank);
-        }
+        var totalJoltage = batteryBanks.Sum(x => FindJoltage(x, 2));
 
         return totalJoltage.ToString();
     }
 
     public string Part2(string input)
     {
-        throw new NotImplementedException();
+        var batteryBanks = ParseInput(input);
+        var totalJoltage = batteryBanks.Sum(x => FindJoltage(x, 12));
+
+        return totalJoltage.ToString();
+    }
+
+    private static long FindJoltage(List<int> batteryBank, int batteryCount)
+    {
+        var biggestJolt = new List<int>(new int[batteryCount]);
+        var lastBatteryIndex = -1;
+
+        for (var joltingIndex = 0; joltingIndex < batteryCount; joltingIndex++)
+        {
+            var maxBatteryIndex = batteryBank.Count - batteryCount + joltingIndex + 1;
+
+            for (var batteryIndex = lastBatteryIndex + 1; batteryIndex < maxBatteryIndex; batteryIndex++)
+            {
+                var thisBatteryValue  = batteryBank[batteryIndex];
+                if (thisBatteryValue <= biggestJolt[joltingIndex]) continue;
+                
+                biggestJolt[joltingIndex] = thisBatteryValue;
+                lastBatteryIndex = batteryIndex;
+            }
+        }
+        
+        return long.Parse(string.Concat(biggestJolt.Select(x => x.ToString())));
     }
 }
--- Aoc25.Test/Day3.cs
+++ Aoc25.Test/Day3.cs
@@ -19,4 +19,22 @@ public class Day3
         // Assert
         Assert.Equal("357", day.Part1(input));
     }
+    
+    [Fact]
+    public void should_give_expected_result_for_mini_input_part2()
+    {
+        // Arrange
+        var input = """
+                    987654321111111
+                    811111111111119
+                    234234234234278
+                    818181911112111
+                    """;
+
+        // Act
+        var day = new AoC25.Days.Day3();
+
+        // Assert
+        Assert.Equal("3121910778619", day.Part2(input));
+    }
 }
