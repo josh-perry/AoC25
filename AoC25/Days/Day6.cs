3963ee1b8a9ee818e026ea61b626f9568ec6c1ae
Josh Perry <joshperry@outlook.com>
2025-12-06 20:52:00 +0000

    Day 6 - part 2
    Don't even talk to me about this one, I don't know.

--- AoC25/Days/Day6.cs
+++ AoC25/Days/Day6.cs
@@ -6,7 +6,7 @@ public partial class Day6 : IDay
 {
     public int Day => 6;
 
-    [GeneratedRegex(@"(?<number>\d+)|(?<operator>\*|\+)")]
+    [GeneratedRegex(@"(?<number>\s*\d+\s*) ?|(?<operator>\*|\+)")]
     private static partial Regex ProblemRegex();
     
     public string Part1(string input)
@@ -32,12 +32,61 @@ public partial class Day6 : IDay
 
     public string Part2(string input)
     {
-        throw new NotImplementedException();
+        var problems = ParseInputPart2(input);
+
+        foreach (var problem in problems)
+        {
+            var longestStringValue = problem.StringValues.Max(x => x.Length);
+            problem.Values = new();
+
+            for (var index = 0; index < longestStringValue; index++)
+            {
+                var s = "";
+                
+                foreach (var value in problem.StringValues)
+                {
+                    if (index >= value.Length)
+                    {
+                        continue;
+                    }
+                    
+                    s += value[index];
+                }
+
+                if (string.IsNullOrWhiteSpace(s))
+                {
+                    continue;
+                }
+
+                problem.Values.Add(long.Parse(s));
+            }
+            
+            problem.Values.Reverse();
+        }
+
+        long total = 0;
+        
+        foreach (var problem in problems)
+        {
+            switch (problem.Operator)
+            {
+                case '+':
+                    total += problem.Values.Sum();
+                    break;
+                case '*':
+                    total += problem.Values.Aggregate((x, y) => x * y);
+                    break;
+            }
+        }
+
+        return total.ToString();
     }
 
     private class Problem
     {
         public List<long> Values { get; set; } = new();
+        
+        public List<string> StringValues { get; set; } = new();
 
         public char Operator { get; set; } = ' ';
     }
@@ -70,6 +119,60 @@ public partial class Day6 : IDay
                     problem.Values.Add(long.Parse(match.Groups["number"].Value));
                 if (match.Groups["operator"].Success)
                     problem.Operator = Convert.ToChar(match.Groups["operator"].Value);
+            }
+        }
+
+        return problems;
+    }
+    
+    private List<Problem> ParseInputPart2(string input)
+    {
+        var problems = new List<Problem>();
+        var operatorRegex = new Regex(@"\+|\*");
+        var columnStartIndexes = new Dictionary<int, int>();
+        var operators = new Dictionary<int, char>();
+        
+        foreach (var line in input.Split(Environment.NewLine))
+        {
+            var matches = operatorRegex.Matches(line);
+            var column = 0;
+
+            foreach (Match match in matches)
+            {
+                columnStartIndexes[column] = match.Index;
+                operators[column] = match.Value[0];
+                column++;
+            }
+        }
+        
+        foreach (var line in input.Split(Environment.NewLine))
+        {
+            if (operatorRegex.IsMatch(line))
+            {
+                continue;
+            }
+
+            for (var columnIndex = 0; columnIndex < columnStartIndexes.Count; columnIndex++)
+            {
+                var columnLength = columnIndex == columnStartIndexes.Count - 1
+                    ? line.Length -  columnStartIndexes[columnIndex]
+                    : columnStartIndexes[columnIndex + 1] - columnStartIndexes[columnIndex] - 1;
+                
+                var columnString = line.Substring(columnStartIndexes[columnIndex], columnLength);
+                
+                var r = new Regex($"(?<number>.{{{columnLength}}})");
+
+                var match = r.Match(columnString);
+
+                if (columnIndex > problems.Count - 1)
+                {
+                    problems.Add(new Problem());
+                    problems[columnIndex].Operator = operators[columnIndex];
+                }
+
+                var problem =  problems[columnIndex];
+                var n = match.Groups["number"].Value;
+                problem.StringValues.Add(n);
             }
         }
 
--- Aoc25.Test/Day6.cs
+++ Aoc25.Test/Day6.cs
@@ -19,4 +19,22 @@ public class Day6
         // Assert
         Assert.Equal("4277556", day.Part1(input));
     }
+    
+    [Fact]
+    public void should_give_expected_result_for_mini_input_part2()
+    {
+        // Arrange
+        var input = """
+                    123 328  51 64 
+                     45 64  387 23 
+                      6 98  215 314
+                    *   +   *   +  
+                    """;
+
+        // Act
+        var day = new AoC25.Days.Day6();
+
+        // Assert
+        Assert.Equal("3263827", day.Part2(input));
+    }
 }
