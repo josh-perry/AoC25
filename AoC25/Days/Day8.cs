5f73029bd74cf2e1720a4211357d8e249ab0f131
Josh Perry <joshperry@outlook.com>
2025-12-08 20:07:28 +0000

    Day 8 - part 1

---
+++ AoC25/Days/Day8.cs
@@ -0,0 +1,116 @@
+using System.Text.RegularExpressions;
+
+namespace AoC25.Days;
+
+public class Day8 : IDay
+{
+    public int Day => 8;
+    
+    public string Part1(string input)
+    {
+        var junctionBoxes = ParseInput(input);
+        var circuits = new List<Circuit>();
+
+        var pairs = new List<(JunctionBox a, JunctionBox b, double distance)>();
+        for (var i = 0; i < junctionBoxes.Count; i++)
+        {
+            for (var j = i + 1; j < junctionBoxes.Count; j++)
+            {
+                var distance = GetDistance(junctionBoxes[i], junctionBoxes[j]);
+                pairs.Add((junctionBoxes[i], junctionBoxes[j], distance));
+            }
+        }
+
+        var connectionsMade = 0;
+
+        foreach (var (a, b, distance) in pairs.OrderBy(p => p.distance))
+        {
+            if (a.Circuit is not null && b.Circuit is not null)
+            {
+                if (a.Circuit != b.Circuit)
+                {
+                    var oldCircuit = b.Circuit;
+                    circuits.Remove(oldCircuit);
+
+                    foreach (var box in oldCircuit.JunctionBoxes)
+                    {
+                        box.Circuit = a.Circuit;
+                        a.Circuit.JunctionBoxes.Add(box);
+                    }
+                }
+            }
+            else if (a.Circuit is not null)
+            {
+                b.Circuit = a.Circuit;
+                a.Circuit.JunctionBoxes.Add(b);
+            }
+            else if (b.Circuit is not null)
+            {
+                a.Circuit = b.Circuit;
+                b.Circuit.JunctionBoxes.Add(a);
+            }
+            else
+            {
+                var circuit = new Circuit();
+                circuits.Add(circuit);
+                
+                a.Circuit = circuit;
+                b.Circuit = circuit;
+                
+                circuit.JunctionBoxes.Add(a);
+                circuit.JunctionBoxes.Add(b);
+            }
+
+            connectionsMade++;
+
+            if (connectionsMade == 1000)
+            {
+                break;
+            }
+        }
+
+        var biggest = circuits.OrderByDescending(x => x.JunctionBoxes.Count).Take(3);
+        var total = biggest.Aggregate(1, (current, circuit) => current * circuit.JunctionBoxes.Count);
+        return total.ToString();
+    }
+
+    public string Part2(string input)
+    {
+        return "";
+    }
+
+    private List<JunctionBox> ParseInput(string input)
+    {
+        var regex = new Regex(@"(?<x>\d+),(?<y>\d+),(?<z>\d+)");
+        var junctionBoxes = new List<JunctionBox>();
+        
+        foreach (var line in input.Split(Environment.NewLine))
+        {
+            var match = regex.Match(line);
+
+            var x = int.Parse(match.Groups["x"].Value);
+            var y = int.Parse(match.Groups["y"].Value);
+            var z = int.Parse(match.Groups["z"].Value);
+            
+            junctionBoxes.Add(new(x, y, z));
+        }
+        
+        return junctionBoxes;
+    }
+
+    private double GetDistance(JunctionBox a, JunctionBox b) => 
+        Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2) + Math.Pow(a.Z - b.Z, 2));
+
+    private int GetDistanceSquared(JunctionBox a, JunctionBox b) => 
+        (a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y) + (a.Z - b.Z) * (a.Z - b.Z);
+
+    private record Circuit
+    {
+        public HashSet<JunctionBox> JunctionBoxes { get; set; } = new();
+    };
+
+    private record JunctionBox(int X, int Y, int Z)
+    {
+        public Circuit? Circuit { get; set; }
+    };
+}
---
+++ Aoc25.Test/Day8.cs
@@ -0,0 +1,38 @@
+namespace Aoc25.Test;
+
+public class Day8
+{
+    [Fact]
+    public void should_give_expected_result_for_mini_input_part1()
+    {
+        // Arrange
+        var input = """
+                    162,817,812
+                    57,618,57
+                    906,360,560
+                    592,479,940
+                    352,342,300
+                    466,668,158
+                    542,29,236
+                    431,825,988
+                    739,650,466
+                    52,470,668
+                    216,146,977
+                    819,987,18
+                    117,168,530
+                    805,96,715
+                    346,949,466
+                    970,615,88
+                    941,993,340
+                    862,61,35
+                    984,92,344
+                    425,690,689
+                    """;
+
+        // Act
+        var day = new AoC25.Days.Day8();
+
+        // Assert
+        Assert.Equal("40", day.Part1(input));
+    }
+}
